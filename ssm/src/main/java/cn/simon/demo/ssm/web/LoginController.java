32727ee11de90926b844daa6dcc1a0c942185147
simon <simon@SIMON-PC>
2017-07-20 16:18:13 +0800

    test src append

---
+++ ssm/src/main/java/cn/simon/demo/ssm/web/LoginController.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright ©2017 www.uinpay.cn Technology Co.,Ltd.All Rights Reserved
+ * cn.simon.demo.ssm.web.TestController.java
+ * zxy@uinpay.cn 2017年7月15日
+ */
+package cn.simon.demo.ssm.web;
+/**
+ * @autor: zxy@uinpay.cn
+ * @desc : ...
+ */
+public class LoginController {
+	
+}
